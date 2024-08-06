6906d476fc8322e01b0b09854d254d5a25fcf79e
jansvo <svoboda.honzik@post.cz>
2024-08-06 13:38:32 +0200

    better visuals in jupyter

--- detection_pipeline.py
+++ detection_pipeline.py
@@ -1,7 +1,22 @@
 
 from dataset_loading import CocoLoader
 
+'''
+Main pipeline logic. 
+load datasets
+load detection models
+load segmentation models
 
+test detection on dataset, comapre with GT bboxes
+test segmentation on dataset, compare with GT masks
+
+Goal: 
+    - metrics for detection
+    - metrics for segmentation
+    - output tables, output visualizations, saving BBoxes, masks, etc.
+    - eventually another script to load the results without rerunning
+    - reproducible code here, examples of usage
+'''
 if __name__ == "__main__": 
     #can currently be seen in seg.ipynb
 
--- run_prob.py
+++ run_prob.py
@@ -6,9 +6,20 @@ from prob.config import get_cfg
 import torchvision.transforms as T
 
 
-def load_checkpoint(checkpoint_path):
+def load_model(checkpoint_path:str, model:str='prob'):
     '''
-    TODO:needs work, load the correct model, conf?
+    Load the specified model from the checkpoint path
+    '''
+    if model == "prob":
+        model = prob_load_checkpoint(checkpoint_path)
+    else:
+        raise ValueError("Invalid model name")
+    return model
+
+
+def prob_load_checkpoint(checkpoint_path:str,cfg:dict):
+    '''
+    TODO: needs work, load the correct model, conf?
     '''
     cfg = get_cfg()
     cfg.merge_from_file(checkpoint_path)  # Load the checkpoint config
@@ -17,13 +28,12 @@ def load_checkpoint(checkpoint_path):
     return model
 
 
-def preprocess_image(images:list):
+def preprocess_image(image):
     '''
     Currently not needed nor operational
     Replace transformations with different transforms? or delete?
     '''
-    image = Image.open(image_path).convert('RGB')
-    transform = T.Compose([
+    transform = T.Compose([ 
         T.Resize((800, 800)),
         T.ToTensor(),
         T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
@@ -31,25 +41,31 @@ def preprocess_image(images:list):
     transformed = transform(image).unsqueeze(0)
     return None
 
-def run_inference(model,image:tensor,preprocessing_fn:callable):
+def run_inference_one(model,image_tensor:tensor,transforms:list):
     ''' 
     Common inference function for all models
     '''
     model.eval()
-    image = preprocess_image(image,preprocessing_fn)
+    image_tensor_processes = preprocess_image(image_tensor,transforms)
     with torch.no_grad():
         output = model(image)
     return output
 
-def detect_object(image, model):
-    model = load_checkpoint(checkpoint_path)
-    output = run_inference(model, image_path)
-    # Process the output to get the detected objects
-    # ...
-    return detected_objects
 
-def detect_objects(model_path:str, images:list):
-    model = load_checkpoint(model_path)
+def detect_objects(model,images:list):
+    predicted_bboxes = []
     for image in images:
         bboxes = run_inference(model)
-    return detected_objects
+        predicted_bboxes.append(bboxes)
+    return predicted_bboxes
+
+
+if __name__ == "__main__": 
+    #TODO: minimal running example, load 1 image for test?
+    model="prob"
+    model_path = "path/to/model" #TODO:fill
+
+    model = load_model(model_path,model)
+    images=[] #mock list of images here
+    predicted_bboxes = detect_objects(model,images)
+    #visualize into a file???? (no code yet)
