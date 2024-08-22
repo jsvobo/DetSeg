47517ddd915a3984ad1f3b7f997bdca495a0b531
jansvo <svoboda.honzik@post.cz>
2024-08-22 12:50:45 +0200

    testing, jupyter notebooks tested, updated and pruned

--- test_all.py
+++ test_all.py
@@ -1,15 +1,11 @@
 from testing.cuda_test import test_cuda
 from datasets.dataset_loading import test_coco_loading
+from segmentation_models.sam1_wrapper import test_sam_wrappers
+from evaluator import test_evaluator
 
-if __name__ == "__main__": 
-    ''' 
-    test pipeline
-    test loading all datasets
-    test cuda
-    test loading/saving?
-    test everything I can?
-    '''
 
+if __name__ == "__main__":
     test_cuda()
     test_coco_loading()
-    
+    test_sam_wrappers()
+    test_evaluator()
